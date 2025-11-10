d2e1f3a69ec7a841332c014921d3bfd2e0b8c051
Nico Heinola <nico.heinola@gmail.com>
2025-11-10 20:34:43 +0200

    Initialize project structure with environment configuration, routing, and authentication middleware

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI
+import uvicorn
+from routes.index import router as index_router
+
+app = FastAPI()
+
+# Include routes
+app.include_router(index_router)
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    HOST: str = os.getenv("HOST", "")
+    PORT: int = int(os.getenv("PORT", ""))
+
+    uvicorn.run(app, host=HOST, port=PORT)
