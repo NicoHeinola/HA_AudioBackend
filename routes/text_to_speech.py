97fb81d5aaff4de14e60c3bcb28d1bb9a4bb2b74
Nico Heinola <nico.heinola@gmail.com>
2025-11-10 21:46:26 +0200

    Update environment configuration and fix token usage in text-to-speech route

--- routes/text_to_speech.py
+++ routes/text_to_speech.py
@@ -19,8 +19,9 @@ def text_to_speech(token: str = require_auth(), body: dict = Body(...)):
         ha_host: str = os.getenv("HOME_ASSISTANT_HOST", "")
         ha_port: str = os.getenv("HOME_ASSISTANT_PORT", "")
         ha_url: str = f"{ha_host}:{ha_port}"
+        ha_token: str = os.getenv("HOME_ASSISTANT_TOKEN", "")
 
-        tts_api = HATextToSpeechAPI(ha_url, token)
+        tts_api = HATextToSpeechAPI(ha_url, ha_token)
         tts_result = tts_api.convert_text_to_speech(engine_id, message)
 
         return Response(content=tts_result["content"], media_type=f"audio/{tts_result['format']}")
